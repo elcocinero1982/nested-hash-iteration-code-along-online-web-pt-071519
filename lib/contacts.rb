9e2ef162c5b4e20529a335b36bb6329ee6d70082
jose luis <cortezluisjose@gmail.com>
2019-07-31 23:21:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,9 +14,7 @@ require 'pry'
   #   }
   # }
   
-def remove_strawberry(contacts)
 
-end
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
